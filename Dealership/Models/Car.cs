45b4ccde2aed72e9ef54a27b979ab739c2017971
Eric Endsley <eric.endsley4@gmail.com>
2020-12-15 11:06:39 -0800

    create form to add car for sale info to list of cars and display
    Co-authored-by: Agata Kolodziej <agatakolohe@gmail.com>
    Co-authored-by: Callie Stump <callie@stu.mp>

--- Dealership/Models/Car.cs
+++ Dealership/Models/Car.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+
 namespace Dealership.Models
 {
   public class Car
@@ -5,21 +8,27 @@ namespace Dealership.Models
     public string MakeModel { get; set; }
     public int Price { get; set; }
     public int Miles { get; set; }
+    private static List<Car> _instances = new List<Car> { };
 
     public Car(string makeModel, int price, int miles)
     {
       MakeModel = makeModel;
       Price = price;
       Miles = miles;
+      _instances.Add(this);
+    }
+    public static List<Car> GetAll()
+    {
+      return _instances;
     }
 
-    public bool WorthBuying(int maxPrice)
-    {
-      return (Price <= maxPrice);
-    }
-    public static string MakeSound(string sound)
-    {
-      return "Our cars sound like " + sound;
-    }
+    // public bool WorthBuying(int maxPrice)
+    // {
+    //   return (Price <= maxPrice);
+    // }
+    // public static string MakeSound(string sound)
+    // {
+    //   return "Our cars sound like " + sound;
+    // }
   }
 }
--- Dealership/Program.cs
+++ Dealership/Program.cs
@@ -1,43 +1,20 @@
-using System;
-using System.Collections.Generic;
-using Dealership.Models;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
 
 namespace Dealership
 {
   public class Program
   {
-    public static void Main()
+    public static void Main(string[] args)
     {
-      Car volkswagen = new Car("1974 Volkswagen Thing", 1100, 368792);
-      Car yugo = new Car("1980 Yugo Koral", 700, 56000);
-      Car ford = new Car("1988 Ford Country Squire", 1400, 239001);
-      Car amc = new Car("1976 AMC Pacer", 400, 198000);
+      var host = new WebHostBuilder()
+        .UseKestrel()
+        .UseContentRoot(Directory.GetCurrentDirectory())
+        .UseIISIntegration()
+        .UseStartup<Startup>()
+        .Build();
 
-      List<Car> Cars = new List<Car>() { volkswagen, yugo, ford, amc};
-
-      Console.WriteLine("Enter maximum price: ");
-      string stringMaxPrice = Console.ReadLine();
-      int maxPrice = int.Parse(stringMaxPrice);
-
-      List<Car> CarsMatchingSearch = new List<Car>(0);
-
-      ford.Price = 100;
-
-      foreach (Car automobile in Cars)
-      {
-        if (automobile.WorthBuying(maxPrice))
-        {
-          CarsMatchingSearch.Add(automobile);
-        }
-      }
-
-      foreach(Car automobile in CarsMatchingSearch)
-      {
-        Console.WriteLine("----------------------");
-        Console.WriteLine(automobile.MakeModel);
-        Console.WriteLine(automobile.Miles + " miles");
-        Console.WriteLine("$" + automobile.Price);
-      }
+      host.Run();
     }
   }
 }
