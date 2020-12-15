ce927c415b75dc81067722534929caeb31d5057d
Eric Endsley <eric.endsley4@gmail.com>
2020-12-15 09:55:37 -0800

    add all files for separation of project, add configurations to startup
    Co-authored-by: Agata Kolodziej <agatakolohe@gmail.com>
    Co-authored-by: Callie Stump <callie@stu.mp>

---
+++ Dealership/Models/Car.cs
@@ -0,0 +1,25 @@
+namespace Dealership.Models
+{
+  public class Car
+  {
+    public string MakeModel { get; set; }
+    public int Price { get; set; }
+    public int Miles { get; set; }
+
+    public Car(string makeModel, int price, int miles)
+    {
+      MakeModel = makeModel;
+      Price = price;
+      Miles = miles;
+    }
+
+    public bool WorthBuying(int maxPrice)
+    {
+      return (Price <= maxPrice);
+    }
+    public static string MakeSound(string sound)
+    {
+      return "Our cars sound like " + sound;
+    }
+  }
+}
